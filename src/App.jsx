24034ffb224ddc939cc01bc6707745beed0e4ad7
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-14 18:09:32 -0300

    Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserStorage } from './UserContext';
+
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import User from './Components/User/User';
 
 function App() {
   return (
@@ -15,8 +18,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-          <Route path="/conta" element={<p>Conta</p>} />
+          <Route path="login/*" element={<Login />} />
+          <Route
+						path="conta/*"
+						element={
+						<ProtectedRoute>
+							<User />
+							</ProtectedRoute>
+					} />
         </Routes>
         <Footer />
 			</UserStorage>
--- src/UserContext.js
+++ src/UserContext.js
@@ -68,6 +68,8 @@ export const UserStorage = ({ children }) => {
 				} finally {
 					setLoading(false);
 				}
+			} else {
+				setLogin(false);
 			}
 		}
 		autoLogin();
