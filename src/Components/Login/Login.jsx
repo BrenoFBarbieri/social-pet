479bc6f0ee9848abfe14c00ce6007c59f48bb04d
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-14 16:47:33 -0300

    Login background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+
+import styles from './Login.module.css';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+
 import { UserContext } from '../../UserContext';
 
 const Login = () => {
@@ -11,14 +14,16 @@ const Login = () => {
 
 	if(login === true) return <Navigate to="/conta"  />
 	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<LoginForm />} />
-				<Route path="/criar" element={<LoginCreate />} />
-				<Route path="/perdeu" element={<LoginPasswordLost />} />
-				<Route path="/resetar" element={<LoginPasswordReset/>} />
-			</Routes>
-		</div>
+		<section className={styles.login}>
+			<div className={styles.forms}>
+				<Routes>
+					<Route path="/" element={<LoginForm />} />
+					<Route path="/criar" element={<LoginCreate />} />
+					<Route path="/perdeu" element={<LoginPasswordLost />} />
+					<Route path="/resetar" element={<LoginPasswordReset/>} />
+				</Routes>
+			</div>
+		</section>
 	)
 }
 
