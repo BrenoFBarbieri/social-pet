0386979d11c5314f5852baccf912088f4d075631
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-18 19:00:22 -0300

    Image Skeleton

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "../Helper/Image";
 
 import styles from "./FeedPhotosItem.module.css";
 
@@ -9,7 +10,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
 	return (
 		<li className={styles.photo} onClick={handleClick}>
-			<img src={photo.src} alt={photo.title} />
+			<Image src={photo.src} alt={photo.title} />
 			<span>{photo.acessos}</span>
 		</li>
 	);
