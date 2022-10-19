654a79343404067770306696c17214c9acdec5b7
Breno Barbieri <breno.fbarbieri@gmail.com>
2022-10-19 17:15:42 -0300

    User stats graph component

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../utils/api";
+import Error from "../Helper/Error";
+
 import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-	return (
-		<div>
-			<Head title="Estatísticas" />
-			UserStats
-		</div>
-	);
+	const { data, loading, error, request } = useFetch();
+
+	React.useEffect(() => {
+		async function getData() {
+			const { url, options } = STATS_GET();
+			await request(url, options);
+		}
+		getData();
+	}, [request]);
+
+	if (loading) return <Loading />;
+	if (error) return <Error error={error} />;
+	if (data)
+		return (
+			<div>
+				<Head title="Estatísticas" />
+				<UserStatsGraphs data={data} />
+			</div>
+		);
+	else return null;
 };
 
 export default UserStats;
